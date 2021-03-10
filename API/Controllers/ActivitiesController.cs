ef37d70db749e6e9e182a2e418fac95b57fec899
Ammar Mičijević <ammar.micijevic@edu.fit.ba>
2021-03-11 00:39:16 +0100

    Last update

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,34 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext context;
+
+        public ActivitiesController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public List<Activity> GetActivities()
+        {
+            return context.Activities.ToList();
+        }
+        [HttpGet("{id}")]
+        public Activity GetActivity(Guid id)
+        {
+            return context.Activities.Find(id);
+        }
+    }
+}
+
+
---
+++ API/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API
+{
+    public class Program
+    {
+        public async static Task Main(string[] args)
+        {
+            var Host = CreateHostBuilder(args).Build();
+            using var scope = Host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            try
+            {
+                var context = services.GetRequiredService<DataContext>();
+                await context.Database.MigrateAsync();
+                await Seed.SeedData(context);
+            }
+            catch (Exception)
+            {
+
+            }
+            await Host.RunAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
